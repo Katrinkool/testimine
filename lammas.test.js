94b8f64ad2ff8f764c19e6ccb76f7f4e10e2b333
Tehnikumi Opilane <katrin.kalberg@tptlive.ee>
2020-01-25 14:55:39 +0200

    testlammas

---
+++ lammas.test.js
@@ -0,0 +1,7 @@
+const Lammas =require('./lammas');
+
+test ('create Lammas with name dolly, check that name is dolly', ()=>{
+ const l = new Lammas("dolly");
+ expect (l.getName()).toBe("dolly");
+    }
+)
--- sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(value).toBe(3);
-});
